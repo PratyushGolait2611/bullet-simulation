17564ea4038baa2a8e66f14854cffcf78cce6af8
PratyushGolait2611 <69040672+PratyushGolait2611@users.noreply.github.com>
2020-10-01 13:45:47 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,40 +1,54 @@
+// Creating variables
 var bullet, wall;
 var speed, weight,thickness;
 
-
-
-
 function setup() {
+  //Creating background
   createCanvas(1600,400);
+  
+  // To get random speed
   speed = random(223,321);
+  
+  //To get random weight
   weight =  random(30,52);
+  
+  //To get random thickness
   thickness = random(22,83)
+  
+  // Creating a bullet sprite
   bullet = createSprite(50, 200, 50, 50);
+  
+  // Creating a wall sprite
   wall = createSprite(1200, 200, 60, height/2);
+  
+  // Giving color to the  wall
   wall.shapeColor= rgb(80,80,80);
 }
 
 function draw() {
-  background(255,255,255);  
+  // Background
+  background(255,255,255); 
+  
+  //Giving x-axis velocity to bullet
   bullet.velocityX=speed;
-  if(hasCollided(bullet, wall))
-  {
-bullet.velocityX=0;
-var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
-if(damage>10)
-{
-  wall.shapeColor=color(255,0,0);
-}
-if(damage<10)
-{
-  wall.shapeColor=color(0,255,0)
-}
+  
+  // Function that detects collision between bullet and wall
+  if(hasCollided(bullet, wall)){
+  bullet.velocityX=0;
+  var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+  if(damage>10){
+    wall.shapeColor=color(255,0,0);
+    }
+  if(damage<10){
+    wall.shapeColor=color(0,255,0)
+    }
   }
+  
   drawSprites();
 }
 
 
-
+// Collision Function
 function hasCollided(lbullet, lwall)
 {
 bullet.RightEdge=lbullet.x+lbullet.width;
